c3bea4d94edf8b831b5526a2898590110c8929d8
Juan Morales <juanmr2311@gmail.com>
2024-03-14 23:19:10 -0500

    Se crean los métodos para traer la información de contacto

--- ProyectoMVC/Datos/ContactoDatos.cs
+++ ProyectoMVC/Datos/ContactoDatos.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using ProyectoMVC.Models;
 
 namespace ProyectoMVC.Datos
@@ -8,16 +9,22 @@ namespace ProyectoMVC.Datos
     {
         public List<ContactoModel> Listar()
         {
+            //Objeto a devolver
             var oLista = new List<ContactoModel>();
+            //Objeto de conexion
             var con = new Conexion();
+            //Utilizamos la cadena para crear la conexion
             using (var conexion = new SqlConnection(con.getCadenaSQL()))
             {
+                //Se abre la conexion
                 conexion.Open();
+                //Se llama el procedimiento almacenado
                 SqlCommand cmd = new SqlCommand("sp_Listar", conexion);
                 cmd.CommandType = CommandType.StoredProcedure;
 
                 using (var dr = cmd.ExecuteReader())
                 {
+                    //Hace la lectura de los registros obtenidos
                     while (dr.Read())
                     {
                         oLista.Add(new ContactoModel()
@@ -33,7 +40,7 @@ namespace ProyectoMVC.Datos
             return oLista;        
         }
 
-        public ContactoModel Obtener()
+        public ContactoModel Obtener(int IdContacto)
         {
             var oContacto = new ContactoModel();
             var con = new Conexion();
@@ -41,25 +48,99 @@ namespace ProyectoMVC.Datos
             {
                 conexion.Open();
                 SqlCommand cmd = new SqlCommand("sp_Obtener", conexion);
-                cmd.Parameters.AddWithValue("IdContacto")
+                cmd.Parameters.AddWithValue("idContacto", IdContacto);
                 cmd.CommandType = CommandType.StoredProcedure;
 
                 using (var dr = cmd.ExecuteReader())
                 {
                     while (dr.Read())
                     {
-                        oLista.Add(new ContactoModel()
-                        {
-                            IdContacto = Convert.ToInt32(dr["IdContacto"]),
-                            Nombre = dr["Nombre"].ToString(),
-                            Telefono = dr["Telefono"].ToString(),
-                            Correo = dr["Correo"].ToString(),
-                        });
+                        oContacto.IdContacto = Convert.ToInt32(dr["IdContacto"]);
+                        oContacto.Nombre = dr["Nombre"].ToString();
+                        oContacto.Telefono = dr["Telefono"].ToString();
+                        oContacto.Correo = dr["Correo"].ToString();                        
                     }
                 }
             }
-            return oLista;
+            return oContacto;
         }
 
+        public bool Guardar(ContactoModel oContacto)
+        {
+            bool respuesta;
+            try
+            {
+                var con = new Conexion();
+                using (var conexion = new SqlConnection(con.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Guardar", conexion);
+                    cmd.Parameters.AddWithValue("Nombre", oContacto.Nombre);
+                    cmd.Parameters.AddWithValue("Telefono", oContacto.Telefono);
+                    cmd.Parameters.AddWithValue("Correo", oContacto.Correo);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                respuesta = true;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                respuesta = false;
+            }
+            return respuesta;
+        }
+
+        public bool Editar(ContactoModel oContacto)
+        {
+            bool respuesta;
+            try
+            {
+                var con = new Conexion();
+                using (var conexion = new SqlConnection(con.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Editar", conexion);
+                    cmd.Parameters.AddWithValue("idContacto", oContacto.IdContacto);
+                    cmd.Parameters.AddWithValue("Nombre", oContacto.Nombre);
+                    cmd.Parameters.AddWithValue("Telefono", oContacto.Telefono);
+                    cmd.Parameters.AddWithValue("Correo", oContacto.Correo);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                respuesta = true;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                respuesta = false;
+            }
+            return respuesta;
+        }
+
+
+        public bool Eliminar(int IdContacto)
+        {
+            bool respuesta;
+            try
+            {
+                var con = new Conexion();
+                using (var conexion = new SqlConnection(con.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Eliminar", conexion);
+                    cmd.Parameters.AddWithValue("idContacto", IdContacto);                  
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                respuesta = true;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                respuesta = false;
+            }
+            return respuesta;
+        }
     }
 }
