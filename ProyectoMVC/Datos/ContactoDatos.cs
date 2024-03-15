c77a0315a733d87130d5a881d48f67608e6ac56c
Juan Morales <juanmr2311@gmail.com>
2024-03-14 23:04:11 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoMVC/Datos/ContactoDatos.cs
@@ -0,0 +1,65 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using ProyectoMVC.Models;
+
+namespace ProyectoMVC.Datos
+{
+    public class ContactoDatos
+    {
+        public List<ContactoModel> Listar()
+        {
+            var oLista = new List<ContactoModel>();
+            var con = new Conexion();
+            using (var conexion = new SqlConnection(con.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Listar", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oLista.Add(new ContactoModel()
+                        {
+                            IdContacto = Convert.ToInt32(dr["IdContacto"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Telefono = dr["Telefono"].ToString(),
+                            Correo = dr["Correo"].ToString(),
+                        });
+                    }
+                }
+            }
+            return oLista;        
+        }
+
+        public ContactoModel Obtener()
+        {
+            var oContacto = new ContactoModel();
+            var con = new Conexion();
+            using (var conexion = new SqlConnection(con.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Obtener", conexion);
+                cmd.Parameters.AddWithValue("IdContacto")
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oLista.Add(new ContactoModel()
+                        {
+                            IdContacto = Convert.ToInt32(dr["IdContacto"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Telefono = dr["Telefono"].ToString(),
+                            Correo = dr["Correo"].ToString(),
+                        });
+                    }
+                }
+            }
+            return oLista;
+        }
+
+    }
+}
---
+++ ProyectoMVC/Models/ContactoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoMVC.Models
+{
+    public class ContactoModel
+    {
+        //Columnas de la tabla Contacto
+        public int IdContacto { get; set; }
+        public string ? Nombre { get; set; }
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; }
+    }
+}
