3a9af6745bb4ff24a7022ec56ae696197ec57013
Juan Morales <juanmr2311@gmail.com>
2024-03-15 00:39:49 -0500

    Se corrige string de conexión y se agrega contenido al listar registros

---
+++ ProyectoMVC/Controllers/MantenedorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ProyectoMVC.Datos;
+using ProyectoMVC.Models;
+
+namespace ProyectoMVC.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        ContactoDatos _ContactoDatos = new ContactoDatos();
+        public IActionResult Listar()
+        {
+            //Muestra la lista de contactos
+            var oLista = _ContactoDatos.Listar();
+            return View(oLista);
+        }
+
+        public IActionResult Guardar()
+        {
+            //Devuelve solo la vista
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guardar(ContactoModel oContacto)
+        {
+            //Recibe un objeto para guardarlo en la db
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var respuesta = _ContactoDatos.Guardar(oContacto);
+            if (respuesta)
+            {
+                return RedirectToAction("Listar");
+            }
+            else
+            {
+            return View();
+            }
+        }
+    }
+}
--- ProyectoMVC/Models/ContactoModel.cs
+++ ProyectoMVC/Models/ContactoModel.cs
@@ -1,11 +1,16 @@
-﻿namespace ProyectoMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoMVC.Models
 {
     public class ContactoModel
     {
         //Columnas de la tabla Contacto
         public int IdContacto { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
         public string ? Nombre { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
         public string? Telefono { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
         public string? Correo { get; set; }
     }
 }
